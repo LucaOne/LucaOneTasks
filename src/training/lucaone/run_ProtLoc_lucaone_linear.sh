788f4eeb493bf5919779a1609abc1207070bdb21
sanyuan.hy <sanyuan.hy@alibaba-inc.com>
2025-01-15 14:53:36 +0800

    feature: optimized code

--- src/training/lucaone/run_ProtLoc_lucaone_linear.sh
+++ src/training/lucaone/run_ProtLoc_lucaone_linear.sh
@@ -148,6 +148,6 @@ python run.py \
   --beta2 0.98 \
   --weight_decay 0.01 \
   --save_steps $save_steps \
-  --max_steps $max_steps
+  --max_steps $max_steps \
   --logging_steps $logging_steps \
   --loss_reduction mean

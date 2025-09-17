619fe54c2cc44a4bceda4731d6007496feeb454c
sanyuan.hy <sanyuan.hy@alibaba-inc.com>
2025-09-17 15:44:53 +0800

    feature: fix bug for predict_*.py

--- src/llm/esm/predict_embedding.py
+++ src/llm/esm/predict_embedding.py
@@ -457,9 +457,9 @@ def predict_embedding(
                 return None, None
         except RuntimeError as e:
             if e.args[0].startswith("CUDA out of memory"):
-                print(f"Failed (CUDA out of memory) on sequence {sample[0]} of length {len(sample[1])}.")
+                print(f"Failed (CUDA out of memory) on sequence {protein_id} of length {len(protein_seq)}.")
                 print("Please reduce the 'truncation_seq_length'")
-            raise Exception(e)
+            # raise Exception(e)
     return None, None
 
 
